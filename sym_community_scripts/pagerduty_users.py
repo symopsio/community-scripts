a25d4937891cfcf1d09776803fd09d7eea9aec55
jtimberman <joshua@symops.io>
2021-03-24 17:37:57 -0600

    add an option to write to a CSV
    This commit adds an option to write the PagerDuty user information to
    a CSV file. It also implements `tabulate` to display the user data as
    a nicely formatted table on stdout. Since we can write to a CSV now,
    users can optionally use that in other streams for whatever they want
    to make of this data.
    
    Signed-off-by: jtimberman <joshua@symops.io>

--- sym_community_scripts/pagerduty_users.py
+++ sym_community_scripts/pagerduty_users.py
@@ -3,10 +3,19 @@
 # License: BSD-3-Clause
 #
 
-import json
 import pdpyras
 import click
+import sys
+import csv
+import io
+from tabulate import tabulate
 
+
+def display(table):
+    print(tabulate(table, headers=["Email", "Name"]))
+
+def session(api_token):
+    return pdpyras.APISession(api_token)
 
 @click.command()
 @click.option(
@@ -14,22 +23,30 @@ import click
 )
 @click.option("--match", help="Show users that match the specified string")
 @click.option("--email-only", is_flag=True, default=False, help="Show only the email addresses")
-def main(token, match, email_only):
+@click.option("--outfile", help="Write output to a CSV file")
+def main(token, match, email_only, outfile):
+    userdata = []
+
     if not token:
         print("You must supply a token, use `--token` or export `PD_API_TOKEN` in your shell")
         exit(1)
 
-    session = pdpyras.APISession(token)
     if match:
         query = {"query": match}
     else:
         query = {}
 
-    for user in session.iter_all("users", params=query):
-        display = [user["email"]]
+    for user in session(token).iter_all("users", params=query):
+        row = [user["email"]]
         if not email_only:
-            display.append(user["name"])
-        print(",".join(display))
+            row.append(user["name"])
+        userdata.append(row)
+
+    display(userdata)
+
+    if outfile:
+        with open(outfile, 'w') as csvfile:
+            csv.writer(csvfile, quoting=csv.QUOTE_ALL).writerows(userdata)
 
 
 if __name__ == "__main__":
